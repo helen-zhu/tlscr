83caa94df2f770374b61e36754b67d37c4eb2af1
helen-zhu <helen.m.zhu@gmail.com>
2022-07-12 10:41:30 -0400

    adding notes

--- homework3.py
+++ homework3.py
@@ -2,6 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse, urlunparse
 
+# beautiful soup has an attribute called source to tell you which line it is
+
+# pathlib is a library for paths
+# takes os.path + other libraries and make one module
+
+# Explain servers, ports, and sockets
+# Port 80 is for webservers (typical convention)
 URL = "https://gvwilson.github.io/tlscr/species-index.html"
 
 response = requests.get(URL)
